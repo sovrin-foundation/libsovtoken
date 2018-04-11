6408e16564ad7cc5ff6f4f2464dfcbda2b6c8416
matt raffel <matt.raffel@evernym.com>
2018-04-11 10:50:13 -0600

    adding comment headers, updated gitignore due to project structure changes

--- libsovtoken/src/api/mod.rs
+++ libsovtoken/src/api/mod.rs
@@ -4,41 +4,121 @@
 //
 //
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn create_payment_address_handler() -> bool {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn list_payment_addresses_handler() -> bool {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn add_request_fees_handler() -> bool  {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn build_payment_txn_handler() -> bool {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn build_fees_txn_handler() -> bool {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn build_get_fees_txn_handler() -> bool {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn build_get_utxo_txn_handler() -> bool {
     return false;
 }
 
+/// Description
+///
+/// #Params
+/// param1: description.
+///
+/// #Returns
+/// description. example if json, etc...
+///
+/// #Errors
+/// description of errors
 #[no_mangle]
 pub extern "C" fn build_mint_txn_handler() -> bool {
     return false;
