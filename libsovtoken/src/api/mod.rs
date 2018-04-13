20c7cdae7062b14ed8a3c03738ee7202b0748091
matt raffel <matt.raffel@evernym.com>
2018-04-13 13:23:27 -0600

    updates all methods to use ErrorCode, since the expectation is this is the standard.

--- libsovtoken/src/api/mod.rs
+++ libsovtoken/src/api/mod.rs
@@ -39,8 +39,8 @@ pub extern "C" fn create_payment_address_handler(command_handle: i32,
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn list_payment_addresses_handler() -> bool {
-    return false;
+pub extern "C" fn list_payment_addresses_handler() -> ErrorCode {
+    return ErrorCode::Success;
 }
 
 /// Description
@@ -54,8 +54,8 @@ pub extern "C" fn list_payment_addresses_handler() -> bool {
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn add_request_fees_handler() -> bool  {
-    return false;
+pub extern "C" fn add_request_fees_handler() -> ErrorCode {
+    return ErrorCode::Success;
 }
 
 /// Description
@@ -69,8 +69,8 @@ pub extern "C" fn add_request_fees_handler() -> bool  {
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn build_payment_txn_handler() -> bool {
-    return false;
+pub extern "C" fn build_payment_txn_handler()-> ErrorCode {
+    return ErrorCode::Success;
 }
 
 /// Description
@@ -84,8 +84,8 @@ pub extern "C" fn build_payment_txn_handler() -> bool {
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn build_fees_txn_handler() -> bool {
-    return false;
+pub extern "C" fn build_fees_txn_handler()-> ErrorCode {
+    return ErrorCode::Success;
 }
 
 /// Description
@@ -99,8 +99,8 @@ pub extern "C" fn build_fees_txn_handler() -> bool {
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn build_get_fees_txn_handler() -> bool {
-    return false;
+pub extern "C" fn build_get_fees_txn_handler()-> ErrorCode {
+    return ErrorCode::Success;
 }
 
 /// Description
@@ -114,8 +114,8 @@ pub extern "C" fn build_get_fees_txn_handler() -> bool {
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn build_get_utxo_txn_handler() -> bool {
-    return false;
+pub extern "C" fn build_get_utxo_txn_handler()-> ErrorCode {
+    return ErrorCode::Success;
 }
 
 /// Description
@@ -129,6 +129,6 @@ pub extern "C" fn build_get_utxo_txn_handler() -> bool {
 /// #Errors
 /// description of errors
 #[no_mangle]
-pub extern "C" fn build_mint_txn_handler() -> bool {
-    return false;
+pub extern "C" fn build_mint_txn_handler()-> ErrorCode {
+    return ErrorCode::Success;
 }
