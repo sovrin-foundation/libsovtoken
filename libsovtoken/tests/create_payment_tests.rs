3c066b5b735552b8645fadb35ac547a3920544ff
matt raffel <matt.raffel@evernym.com>
2018-04-30 09:46:59 -0600

    TOK-104 gets unit tests structured and working correct.  Had to add "rlib" to crate type for unit testing to work

---
+++ libsovtoken/tests/create_payment_tests.rs
@@ -0,0 +1,12 @@
+
+#[allow(unused_variables)]
+#[allow(dead_code)]
+
+extern crate sovtoken;
+
+// this test is just an example of how we can arrange our tests
+// TODO: remove this, replace with a real test
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, 2 + 2);
+}
