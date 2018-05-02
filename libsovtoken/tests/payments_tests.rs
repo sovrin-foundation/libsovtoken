1d73fb71ef1c77b41d8ee9e1873d319e75a608c7
matt raffel <matt.raffel@evernym.com>
2018-05-02 16:08:03 -0600

    TOK-104 adds additional tests

--- libsovtoken/tests/payments_tests.rs
+++ libsovtoken/tests/payments_tests.rs
@@ -9,18 +9,21 @@ extern crate sovtoken;
 extern crate indy;                      // lib-sdk project
 
 use rand::{Rng, random};
+use std::panic;
 
 use sovtoken::logic::payment_address_config::PaymentAddressConfig;
 use sovtoken::logic::payments::{create_payment_address, PAY_INDICATOR, SOVRIN_INDICATOR, PAYMENT_ADDRESS_FIELD_SEP};
 
 static VALID_ADDRESS_LEN: usize = 32;
+static VALID_SEED_LEN: usize = 32;
+static INVALID_SEED_LEN: usize = 19;
 static CHECKSUM_LEN: usize = 4;
 
 // helper methods
-fn rand_string() -> String {
+fn rand_string(length : usize) -> String {
     let s = rand::thread_rng()
             .gen_ascii_chars()
-            .take(VALID_ADDRESS_LEN)
+            .take(length)
             .collect::<String>();
 
     return s;
@@ -31,27 +34,19 @@ fn rand_string() -> String {
 // a fully formatted address is returned.
 #[test]
 fn success_create_payment_with_seed_returns_address() {
-    let seed = rand_string();
-    println!("using seed {} len {}", seed, seed.chars().count());
-
+    let seed = rand_string(VALID_SEED_LEN);
     let config: PaymentAddressConfig = PaymentAddressConfig { seed };
 
     let address = create_payment_address(config);
 
-    println!("got back address {} len {}", address, address.chars().count());
-
+    // got our result, break it up into the individual parts we
+    // expect to find
     let pay_indicator = &address[0..3];
     let first_separator = &address[3..4];
     let sov_indicator = &address[4..7];
     let second_indicator = &address[7..8];
     let result_address = &address[8..40];
     let checksum = &address[40..44];
-
-
-    println!("got pay indicator {} len {}", pay_indicator, pay_indicator.chars().count());
-    println!("got result address {} len {}", result_address, result_address.chars().count());
-    println!("got checksum {} len {}", checksum, checksum.chars().count());
-
 
     assert_eq!(PAY_INDICATOR, pay_indicator, "PAY_INDICATOR not found");
     assert_eq!(PAYMENT_ADDRESS_FIELD_SEP, first_separator, "first PAYMENT_ADDRESS_FIELD_SEP not found");
@@ -68,14 +63,12 @@ fn success_create_payment_with_seed_returns_address() {
 fn success_create_payment_with_no_seed_returns_address() {
 
     let seed = String::new();
-    println!("using seed {} len {}", seed, seed.chars().count());
-
     let config: PaymentAddressConfig = PaymentAddressConfig { seed };
 
     let address = create_payment_address(config);
 
-    println!("got back address {} len {}", address, address.chars().count());
-
+    // got our result, break it up into the individual parts we
+    // expect to find
     let pay_indicator = &address[0..3];
     let first_separator = &address[3..4];
     let sov_indicator = &address[4..7];
@@ -83,17 +76,26 @@ fn success_create_payment_with_no_seed_returns_address() {
     let result_address = &address[8..40];
     let checksum = &address[40..44];
 
-
-    println!("got pay indicator {} len {}", pay_indicator, pay_indicator.chars().count());
-    println!("got result address {} len {}", result_address, result_address.chars().count());
-    println!("got checksum {} len {}", checksum, checksum.chars().count());
-
-
     assert_eq!(PAY_INDICATOR, pay_indicator, "PAY_INDICATOR not found");
     assert_eq!(PAYMENT_ADDRESS_FIELD_SEP, first_separator, "first PAYMENT_ADDRESS_FIELD_SEP not found");
     assert_eq!(SOVRIN_INDICATOR, sov_indicator, "SOVRIN_INDICATOR not found");
     assert_eq!(PAYMENT_ADDRESS_FIELD_SEP, second_indicator, "second PAYMENT_ADDRESS_FIELD_SEP not found");
     assert_eq!(VALID_ADDRESS_LEN, result_address.chars().count(), "address is not 32 bytes");
     assert_eq!(CHECKSUM_LEN, checksum.chars().count(), "checksum is not 4 bytes");
+}
 
+// This test uses an invalid seed length to generate an address. Expectation is an error (panic)
+// is raised.
+#[test]
+fn error_create_payment_with_invalid_seed_len() {
+
+    let result = panic::catch_unwind(|| {
+
+        let seed = rand_string(INVALID_SEED_LEN);
+        let config: PaymentAddressConfig = PaymentAddressConfig { seed };
+
+        let address = create_payment_address(config);
+    });
+
+    assert!(result.is_err(), "create_payment_address did not throw error on invalid seed length");
 }
