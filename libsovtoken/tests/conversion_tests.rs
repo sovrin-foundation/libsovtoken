8ec6499c86473612999dddbe614042c3457c7df9
matt raffel <matt.raffel@evernym.com>
2018-05-02 09:13:21 -0600

    TOK-104 adds one more test and removes a println!

--- libsovtoken/tests/conversion_tests.rs
+++ libsovtoken/tests/conversion_tests.rs
@@ -32,7 +32,7 @@ fn convert_json_str_to_type_test() {
 
     let result: DummyJsonStruct = DummyJsonStruct::from_json(VALID_DUMMY_JSON).unwrap();
 
-    assert_eq!("data", result.field1, "decoding default json failed");
+    assert_eq!(FIELD1_VALUE, result.field1, "decoding default json failed");
 }
 
 // this test creates an oject with the Serialize attribute and expects to get a propertly
@@ -45,9 +45,25 @@ fn convert_obj_to_json_str_test() {
 
     let result: String = instance.to_json().unwrap();
 
-    println!("result is: {} and is_empty: {}", result, result.is_empty());
     assert_ne!(true, result.is_empty(), "convert_obj_to_json_str_test failed to serialize to json string");
     assert_eq!(VALID_DUMMY_JSON, result, "convert_obj_to_json_str_test failed to serialized json did not match result");
+}
+
+
+// this test covers converting an object to json and the resulting json back into an object.  the
+// two objects should contain same values.  they will not be the same object however
+#[test]
+fn convert_obj_to_json_and_back_to_obj_test() {
+
+    let field1: String = FIELD1_VALUE.to_string();
+    let instance: DummyJsonStruct = DummyJsonStruct { field1, };
+
+    let result_str: String = instance.to_json().unwrap();
+
+    let result: DummyJsonStruct = DummyJsonStruct::from_json(result_str.as_str()).unwrap();
+
+    assert_eq!(FIELD1_VALUE, result.field1, "decoding default json failed");
+    assert_eq!(instance.field1, result.field1, "comparison of objects failed");
 }
 
 // this test makes sure our conversion from char * to str works by returning a valid
