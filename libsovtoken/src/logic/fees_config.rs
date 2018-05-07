05296ecff8be9928e9b12707ab289288e2b58a68
cam-parra <ceparra@gmail.com>
2018-05-07 13:43:11 -0600

    TOK-126 so kyle can pair program with us

--- libsovtoken/src/logic/fees_config.rs
+++ libsovtoken/src/logic/fees_config.rs
@@ -2,13 +2,35 @@
 #[allow(unused_imports)]
 
 use serde::{Serialize, Deserialize};
+use serde::lib::HashMap;
 
-type Fee =  (String, u32, String);
+//type Fee =  (String, u32, String);
 
 #[derive(Serialize, Deserialize)]
-pub struct FeesConfig {
-   pub fees: Vec<Fee>,
+pub struct Fees<'a> {
+   pub fees: &'a HashMap<&'static str, u32>,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct Signatures {
+    signatures: HashMap<String,String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Operation {
+    type_op: &'static str,
+    fees: HashMap <&'static str, u32>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SetFeesRequest {
+    type_txn: &'static str,
+    signatures: Signatures,
+    protocolVersion: u32,
+    operation: Operation,
+}
+
+
 
 #[cfg(test)]
 mod fees_config_test {
@@ -17,11 +39,11 @@ mod fees_config_test {
     use std::ffi::CString;
     use utils::ffi_support::{str_from_char_ptr, cstring_from_str};
     use utils::json_conversion::{JsonSerialize};
-    static test_op_json : &'static str = r#"{"fees":[["ThisIsomeBizzareDIdsgivenTOme",1001,"MoreBigBitNums"]]}"#;
+    static test_op_json : &'static str = r#"{"fees":[["ThisIsomeBizzareDIdsgivenTOme",1001]]}"#;
     #[test]
     fn valid_request () {
-        let fee :FeesConfig = FeesConfig{
-            fees : vec![(String::from("ThisIsomeBizzareDIdsgivenTOme"), 1001, String::from("MoreBigBitNums"))],
+        let fee :Fees = Fees {
+            fees : HashMap![(String::from("ThisIsomeBizzareDIdsgivenTOme"), 1001)],
             };
         assert_eq!(fee.to_json().unwrap(), test_op_json);
     }
