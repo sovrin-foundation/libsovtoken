8f40826ee1f9d86b722705bc6a96389c1e3a8601
cam-parra <ceparra@gmail.com>
2018-05-08 08:04:46 -0600

    TOK-126 made code clearer and better

--- libsovtoken/src/logic/fees_config.rs
+++ libsovtoken/src/logic/fees_config.rs
@@ -2,13 +2,13 @@
 #[allow(unused_imports)]
 
 use serde::{Serialize, Deserialize};
-use serde::lib::HashMap;
+use std::collections::HashMap;
 
 //type Fee =  (String, u32, String);
 
 #[derive(Serialize, Deserialize)]
-pub struct Fees<'a> {
-   pub fees: &'a HashMap<&'static str, u32>,
+pub struct Fees {
+   pub fees: HashMap<String, u32>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -18,15 +18,18 @@ pub struct Signatures {
 
 #[derive(Serialize, Deserialize)]
 pub struct Operation {
-    type_op: &'static str,
-    fees: HashMap <&'static str, u32>,
+    #[serde(rename = "type")]
+    type_op: String,
+    #[serde(flatten)]
+    fees: Fees,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct SetFeesRequest {
-    type_txn: &'static str,
+    #[serde(rename = "type")]
+    type_txn: String,
     signatures: Signatures,
-    protocolVersion: u32,
+    protocol_version: u32,
     operation: Operation,
 }
 
@@ -39,12 +42,45 @@ mod fees_config_test {
     use std::ffi::CString;
     use utils::ffi_support::{str_from_char_ptr, cstring_from_str};
     use utils::json_conversion::{JsonSerialize};
-    static test_op_json : &'static str = r#"{"fees":[["ThisIsomeBizzareDIdsgivenTOme",1001]]}"#;
+    static TEST_OP_JSON: &'static str = r#"{"fees":{"ThisIsomeBizzareDIdsgivenTOme":1001}}"#;
+    static TEST_SIGS_JSON: &'static str = r#"{"signatures":{"one":"two","three":"four"}}"#;
+    static TEST_OPS_JSON: &'static str = r#"{"type":"FEE","fees":{"ThisIsomeBizzareDIdsgivenTOme":1001,"ThisIsomeBizzareDIdsgivenTOme1":1001}}"#;
     #[test]
-    fn valid_request () {
+    fn valid_fees () {
+        let mut fees_map = HashMap::new();
+        fees_map.insert(String::from("ThisIsomeBizzareDIdsgivenTOme"), 1001 as u32);
         let fee :Fees = Fees {
-            fees : HashMap![(String::from("ThisIsomeBizzareDIdsgivenTOme"), 1001)],
+            fees: fees_map,
             };
-        assert_eq!(fee.to_json().unwrap(), test_op_json);
+        assert_eq!(fee.to_json().unwrap(), TEST_OP_JSON);
     }
+    #[test]
+    fn valid_signatures () {
+        let mut sig_map = HashMap::new();
+        sig_map.insert(String::from("one"), String::from("two"));
+        sig_map.insert(String::from("three"), String::from("four"));
+
+        let sig : Signatures = Signatures {
+            signatures: sig_map,
+        };
+        assert_eq!(sig.to_json().unwrap(), TEST_SIGS_JSON);
+    }
+    #[test]
+    fn valid_ops () {
+        let mut fees_map = HashMap::new();
+        fees_map.insert(String::from("ThisIsomeBizzareDIdsgivenTOme"), 1001 as u32);
+        fees_map.insert(String::from("ThisIsomeBizzareDIdsgivenTOme1"), 1001 as u32);
+
+        let fee :Fees = Fees {
+            fees: fees_map,
+        };
+        let op : Operation = Operation {
+            type_op: String::from("FEE"),
+            fees: fee,
+        };
+
+        assert_eq!(op.to_json().unwrap(), TEST_OPS_JSON);
+
+    }
+
 }
