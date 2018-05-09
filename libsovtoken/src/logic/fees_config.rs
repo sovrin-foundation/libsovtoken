5a9066f70ba58b7dcc4537e7473c259a0ab3b5d3
cam-parra <ceparra@gmail.com>
2018-05-09 09:02:59 -0600

    TOK-123 no magic nums

--- libsovtoken/src/logic/fees_config.rs
+++ libsovtoken/src/logic/fees_config.rs
@@ -6,6 +6,8 @@ use serde_json::{Value, Error};
 use std::collections::HashMap;
 use logic::request::Request;
 
+const SET_FEES: &str = "20000";
+
 #[derive(Serialize, Deserialize)]
 pub struct Fees {
    pub  fees: HashMap<String, u64>,
@@ -22,13 +24,13 @@ impl SetFeesRequest {
 
     pub fn new(fees: HashMap<String, u64>, did: String) -> Request<SetFeesRequest> {
         let fee = SetFeesRequest {
-            txn_type: "20000",
+            txn_type: SET_FEES,
             fees,
         };
         return Request::new(fee, did);
     }
 
-    pub fn from_fee(fee: Fees, did: String) -> Request<SetFeesRequest> {
+    pub fn from_fee_config(fee: Fees, did: String) -> Request<SetFeesRequest> {
         return SetFeesRequest::new(fee.fees, did);
     }
 }
@@ -47,6 +49,7 @@ mod fees_config_test {
     static TEST_SIGS_JSON: &'static str = r#"{"signatures":{"one":"two","three":"four"}}"#;
     static TEST_OPS_JSON: &'static str = r#"{"type":"FEE","fees":{"ThisIsomeBizzareDIdsgivenTOme":1001,"ThisIsomeBizzareDIdsgivenTOme1":1001}}"#;
 
+    const SET_FEES: &str = "20000";
     // fees_txn_handler requires that a valid fees transaction is serialized. This tests that
     // the serializing structure for fees works correctly
     fn initial_set_fee_request() -> Request<SetFeesRequest> {
@@ -75,11 +78,38 @@ mod fees_config_test {
     fn unsigned_request() {
         assert_set_fee_request(
             json!({
-                "type": "20000",
+                "type": SET_FEES,
                 "fees": {"AesjahdahudgaiuNotARealAKeyygigfuigraiudgfasfhja":10},
             }),
             HashMap::new(),
             |_set_fee_req| {}
+        );
+    }
+    #[test]
+    fn create_request_with_fees_config() {
+        let mut fees_map = HashMap::new();
+        fees_map.insert(String::from("AesjahdahudgaiuNotARealAKeyygigfuigraiudgfasfhja"), 10 as u64);
+        let fees_config = Fees {
+            fees: fees_map.clone()
+        };
+        let request = SetFeesRequest::from_fee_config(fees_config, String::from("EFlzewrfDSfesaiuhgvcxFhhgpeBUddgseaGIUdFU"));
+        assert_eq!(request.operation.fees, fees_map);
+    }
+
+    #[test]
+    fn valid_request() {
+        let mut sigs = HashMap::new();
+        sigs.insert(String::from("afesfghiofFiASaseUFeaeqiwtquDubwr"), String::from("000glgaeht3wFSdnsjBF23taweLDSUH"));
+
+        assert_set_fee_request(
+            json!({
+                "type": SET_FEES,
+                "fees": {"AesjahdahudgaiuNotARealAKeyygigfuigraiudgfasfhja":10},
+            }),
+            sigs,
+            |fees_req| {
+                fees_req.sign("afesfghiofFiASaseUFeaeqiwtquDubwr", "glgaeht3wFSdnsjBF23taweLDSUH").unwrap();
+            }
         );
     }
 }
