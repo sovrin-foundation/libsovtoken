153a4f61f4e21bfb9dc71513d8d3d87fe1fad880
matt raffel <matt.raffel@evernym.com>
2018-05-15 13:27:37 -0600

    TOK-130 cleaning up ptr conversions (and added some comments, please enjoy the slightly silly humor)

--- samples/create_payment_demo/src/main.rs
+++ samples/create_payment_demo/src/main.rs
@@ -18,18 +18,25 @@ use sovtoken::utils::callbacks::{CallbackUtils, TimeoutUtils};
 static POOL_NAME: &str = "pool_1";
 static XTYPE: &str = "default";
 
+/**
+   calls sovtoken to initialize indy-sdk with libsovtoken payment methods
+*/
 fn initialize_libraries() {
     sovtoken_init();
 }
 
 fn setup_wallet(wallet_name: &String) {
 
+    let pool = CString::new(POOL_NAME.to_string()).unwrap();
+    let xtype = CString::new(XTYPE.to_string()).unwrap();
+    let wallet = CString::new(wallet_name.to_string()).unwrap();
+
     let (create_wallet_receiver, create_wallet_command_handle, create_wallet_callback) = CallbackUtils::closure_to_cb_ec();
     let err =
         indy_create_wallet(create_wallet_command_handle,
-                           CString::new(POOL_NAME.to_string()).unwrap().as_ptr(),
-                           CString::new(wallet_name.to_string()).unwrap().as_ptr(),
-                           CString::new(XTYPE.to_string()).unwrap().as_ptr(),
+                           pool.as_ptr(),
+                           wallet.as_ptr(),
+                           xtype.as_ptr(),
                            null(),
                            null(),
                            create_wallet_callback);
@@ -43,11 +50,13 @@ fn setup_wallet(wallet_name: &String) {
 
 fn open_wallet(wallet_name: &String) -> i32 {
 
+    let wallet = CString::new(POOL_NAME.to_string()).unwrap();
+
     let (open_wallet_receiver, open_wallet_command_handle, open_wallet_callback) = CallbackUtils::closure_to_cb_ec_i32();
 
     let err =
         indy_open_wallet(open_wallet_command_handle,
-                         CString::new(wallet_name.to_string()).unwrap().as_ptr(),
+                         wallet.as_ptr(),
                          null(),
                          null(),
                          open_wallet_callback);
@@ -59,13 +68,16 @@ fn open_wallet(wallet_name: &String) -> i32 {
     return wallet_handle;
 }
 
-
+/**
+   Entry point for the create payment addrress demo.  It will setup the environment, create the payment address
+   and prove it was created by doing something.  preferrably with a wow factor and maybe some cool colors
+*/
 fn main() {
 
     println!("initializing libraries");
     initialize_libraries();
 
-    let wallet_name = "payment_test_wallet_123".to_string();
+    let wallet_name = "payment_test_wallet".to_string();
 
     println!("Setting up an wallet called {}....", wallet_name);
     setup_wallet(&wallet_name);
