e8c6c71fd81c0c51e25a6825956b262ec11f70b1
dastardlychimp <darien.hess@evernym.com>
2018-05-15 16:36:32 -0600

    TOK-117 Separate address logic from payments.

--- libsovtoken/src/logic/mod.rs
+++ libsovtoken/src/logic/mod.rs
@@ -3,6 +3,7 @@
 // actually doing the work of libsovtoken
 //
 
+pub mod address;
 pub mod fees_config;
 pub mod fees_req_config;
 pub mod indysdk_api;
@@ -12,4 +13,3 @@ pub mod output_mint_config;
 pub mod payment_address_config;
 pub mod payments;
 pub mod request;
-
