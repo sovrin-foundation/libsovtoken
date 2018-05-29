40d485a0e268a326911722d095b5226bd0000bd1
cam-parra <ceparra@gmail.com>
2018-05-29 13:58:10 -0600

    for usefulness

--- libsovtoken/src/logic/config/get_fees_config.rs
+++ libsovtoken/src/logic/config/get_fees_config.rs
@@ -6,22 +6,26 @@
  */
 use std::collections::HashMap;
 use logic::request::Request;
-
-const SET_FEES: &str = "20000";
+use rand;
+const GET_FEES : String = String::from("20001");
 
 /**
  *  Json config to customize [`build_fees_txn_handler`]
  *
  *  [`build_fees_txn_handler`]: ../../../api/fn.build_fees_txn_handler.html
  */
-#[derive(Serialize, Deserialize)]
-pub struct Fees {
-    pub  fees: HashMap<String, u64>,
-}
-
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub struct getFeesRequest {
     #[serde(rename = "type")]
-    txn_type: &'static str,
-    fees:  HashMap<String, u64>,
+    txn_type: String,
+}
+
+impl getFeesRequest {
+
+    pub fn new(identifiers : String, identifier: String) -> Request<getFeesRequest> {
+        let req = getFeesRequest {
+            txn_type: GET_FEES,
+        };
+        return Request::new(req, identifier);
+    }
 }
