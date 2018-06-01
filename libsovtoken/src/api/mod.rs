96dd0e14b38a550251de42a610f8f42f1703405e
Jerry <jerry@Jerrys-MacBook-Pro.local>
2018-06-01 11:28:00 -0600

    TOK-119 build payment request

--- libsovtoken/src/api/mod.rs
+++ libsovtoken/src/api/mod.rs
@@ -239,13 +239,8 @@ pub extern "C" fn build_payment_req_handler(command_handle: i32,
                                             inputs_json: *const c_char,
                                             outputs_json: *const c_char,
                                             cb: Option<extern fn(command_handle_: i32,
-                                                        err: ErrorCode,
-                                                        payment_req_json: *const c_char) -> ErrorCode>) -> ErrorCode {
-
-
-    println!("move to new line {}", "yes");
-
-    println!("build_payment_req >>> wallet is {:?}", wallet_handle);
+                                                                 err: ErrorCode,
+                                                                 payment_req_json: *const c_char) -> ErrorCode>) -> ErrorCode {
 
     let handle_result = api_result_handler!(< *const c_char >, command_handle, cb);
 
@@ -253,7 +248,7 @@ pub extern "C" fn build_payment_req_handler(command_handle: i32,
         return handle_result(Err(ErrorCode::CommonInvalidParam5));
     }
     if submitter_did.is_null() {
-       return handle_result(Err(ErrorCode::CommonInvalidParam2));
+        return handle_result(Err(ErrorCode::CommonInvalidParam2));
     }
 
     let inputs_json_string = match string_from_char_ptr(inputs_json) {
@@ -264,8 +259,6 @@ pub extern "C" fn build_payment_req_handler(command_handle: i32,
         }
     };
 
-    println!("inputs_json_string = {:?}", inputs_json_string);
-
     trace!("Converting request_json pointer to string");
     let outputs_json_string = match string_from_char_ptr(outputs_json) {
         Some(s) => s,
@@ -275,37 +268,32 @@ pub extern "C" fn build_payment_req_handler(command_handle: i32,
         }
     };
 
-    println!("outputs_json_string = {:?}", outputs_json_string);
+    let inc_did = match string_from_char_ptr(submitter_did) {
+        Some(s) => s,
+        None => {
+            error!("Failed to convert did pointer to string.");
+            return ErrorCode::CommonInvalidParam3;
+        }
+    };
 
-
-    // TODO: remove pay:sov: from the addresses before signing them.
-
-    let the_input: Inputs = serde_json::from_str(&inputs_json_string).unwrap();
-
+    let the_inputs: Inputs = serde_json::from_str(&inputs_json_string).unwrap();
     let the_outputs: Outputs = serde_json::from_str(&outputs_json_string).unwrap();
 
-    let fees = Fees::new(the_input, the_outputs);
-    let fees_signed = fees.sign(CreatePaymentSDK{}, wallet_handle).unwrap();
+    let fees = Fees::new(the_inputs, the_outputs);
+    let fees_signed = fees.sign(CreatePaymentSDK {}, wallet_handle).unwrap();
 
     println!("signed = {:?}", fees_signed);
 
+    let signed_inputs = fees_signed.inputs;
+    let signed_outputs = fees_signed.outputs;
 
+    let payment_request = PaymentRequest::new(signed_outputs, signed_inputs, inc_did);
 
+    let payment_request = payment_request.serialize_to_cstring().unwrap();
 
+    println!("payment_request = {:?}", payment_request);
 
-
-//    let payment_request = PaymentRequest::from_config(outputs_config,inputs_config);
-//    let payment_request = payment_request.serialize_to_cstring().unwrap();
-//
-//    println!("payment_request = {:?}", payment_request);
-//
-//    return handle_result(Ok(payment_request.as_ptr()));
-
-
-
-
-    return ErrorCode::Success;
-
+    return handle_result(Ok(payment_request.as_ptr()));
 }
 
 /// Description
