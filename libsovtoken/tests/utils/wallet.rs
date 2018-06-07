fd6a8a06d94ffc96071db92ce70265d4dd61158a
matt raffel <matt.raffel@evernym.com>
2018-06-07 09:16:34 -0600

    FIXES broken unit tests due to changes in INDY-SDK

--- libsovtoken/tests/utils/wallet.rs
+++ libsovtoken/tests/utils/wallet.rs
@@ -9,6 +9,14 @@ use self::indy::wallet::Wallet;
 
 use std::panic;
 
+static USEFUL_CREDENTIALS : &'static str = r#"
+   {
+       "key": "12345678901234567890123456789012",
+       "rekey": null,
+       "storage": null
+   }
+"#;
+
 /** Creates a wallet and opens it.
  *  Will delete the current wallet if it exists. It will then create and open the wallet.
  *  
@@ -17,11 +25,11 @@ use std::panic;
  */
 pub fn create_wallet(wallet_name : &str) -> i32 {
    let _ = panic::catch_unwind(| | {
-       Wallet::delete(wallet_name, None).unwrap();
+       Wallet::delete(wallet_name, Some(USEFUL_CREDENTIALS)).unwrap();
    });
 
-    Wallet::create("pool_1", wallet_name, None, Some("{}"), None).unwrap();
-    let wallet_id: i32 = Wallet::open(wallet_name, None, None).unwrap();
+    Wallet::create("pool_1", wallet_name, None, None, Some(USEFUL_CREDENTIALS)).unwrap();
+    let wallet_id: i32 = Wallet::open(wallet_name, None, Some(USEFUL_CREDENTIALS)).unwrap();
 
     return wallet_id;
 }
