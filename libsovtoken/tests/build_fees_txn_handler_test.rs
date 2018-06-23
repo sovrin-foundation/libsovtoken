be9c90013903663fd96f0e5160b6e1b029c88974
dastardlychimp <darien.hess@evernym.com>
2018-06-23 11:28:46 -0600

    TOK-214 Add tests for set_fees

--- libsovtoken/tests/build_fees_txn_handler_test.rs
+++ libsovtoken/tests/build_fees_txn_handler_test.rs
@@ -3,16 +3,22 @@
 #![allow(dead_code)]
 #[allow(unused_imports)]
 
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate serde_json;
 extern crate libc;
-
 extern crate sovtoken;
 extern crate rust_indy_sdk as indy;                      // lib-sdk project
+pub mod utils;
 
 use indy::ErrorCode;
-
+use indy::utils::results::ResultHandler;
 use libc::c_char;
-use std::ptr;
+use sovtoken::utils::ffi_support;
 use std::ffi::CString;
+use std::ptr;
+use std::sync::mpsc::{Receiver};
+use std::time::Duration;
+
 
 // ***** HELPER METHODS *****
 extern "C" fn empty_create_payment_callback(command_handle_: i32, err: i32, mint_req_json: *const c_char) -> i32 {
@@ -25,8 +31,19 @@ const COMMAND_HANDLE:i32 = 10;
 const WALLET_ID:i32 = 10;
 static INVALID_OUTPUT_JSON: &'static str = r#"{"totally" : "Not a Number", "bobby" : "DROP ALL TABLES"}"#;
 const CB : Option<extern fn(command_handle_: i32, err: i32, mint_req_json: *const c_char) -> i32 > = Some(empty_create_payment_callback);
+static FAKE_DID : &'static str = "Enfru5LNlA2CnA5n4Hfze";
 
-// ***** UNIT TESTS ****
+
+fn call_set_fees(did: &str, fees_json: serde_json::value::Value) -> (ErrorCode, Receiver<(ErrorCode, String)>) {
+    let (receiver, command_handle, cb) = utils::callbacks::closure_to_cb_ec_string();
+
+    let did_pointer = ffi_support::c_pointer_from_str(did);
+    let fees_pointer = ffi_support::c_pointer_from_string(fees_json.to_string());
+
+    let ec = sovtoken::api::build_set_txn_fees_handler(command_handle, WALLET_ID, did_pointer, fees_pointer, cb);
+
+    return (ErrorCode::from(ec), receiver);
+}
 
 // the build_fees_txn_handler requires a callback and this test ensures that we
 // receive an error when no callback is provided
@@ -50,4 +67,39 @@ fn errors_with_invalid_fees_json() {
     let fees_str_ptr = fees_str.as_ptr();
     let return_error = sovtoken::api::build_set_txn_fees_handler(COMMAND_HANDLE, WALLET_ID, ptr::null(), fees_str_ptr, CB);
     assert_eq!(return_error, ErrorCode::CommonInvalidStructure as i32, "Expecting Valid JSON for 'build_fees_txn_handler'");
+}
+
+#[test]
+fn add_fees_invalid_json_key() {
+    let fees = json!({
+        "1000": 4,
+        "XFER_PUBLIC": 13,
+    });
+
+    let (ec, receiver) = call_set_fees(FAKE_DID, fees);
+    let received = receiver.recv_timeout(Duration::from_millis(300));
+
+    assert_eq!(ErrorCode::CommonInvalidStructure, ec);
+    assert!(received.is_err());
+}
+
+#[test]
+fn add_fees_json() {
+    let fees = json!({
+        "3": 6,
+        "20001": 12
+    });
+    let expected_operation = json!({
+        "type": "20000",
+        "fees": fees,
+    });
+
+    let (ec_initial, receiver) = call_set_fees(FAKE_DID, fees);
+    let (ec_callback, fees_request) = receiver.recv().unwrap();
+
+    let request_value: serde_json::value::Value = serde_json::from_str(&fees_request).unwrap();
+
+    assert_eq!(ErrorCode::Success, ec_initial);
+    assert_eq!(ErrorCode::Success, ec_callback);
+    assert_eq!(&expected_operation, request_value.get("operation").unwrap());
 }
