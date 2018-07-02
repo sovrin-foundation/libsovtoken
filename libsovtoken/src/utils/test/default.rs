38835680dfb8b3ec0161423b7a67e6fa10d2b4a0
dastardlychimp <darien.hess@evernym.com>
2018-07-02 07:01:54 -0600

    Add xfer_payload default functions.

--- libsovtoken/src/utils/test/default.rs
+++ libsovtoken/src/utils/test/default.rs
@@ -8,6 +8,9 @@ use utils::ffi_support::c_pointer_from_string;
 use utils::random::rand_string;
 use utils::constants::txn_types;
 use logic::parsers::common::TXO;
+use logic::input::{Input, Inputs};
+use logic::output::{Output, Outputs};
+use logic::xfer_payload::XferPayload;
 use rust_base58::ToBase58;
 
 pub fn inputs_json_pointer() -> *const c_char {
@@ -19,6 +22,26 @@ pub fn inputs_json_pointer() -> *const c_char {
     ]);
 
     return c_pointer_from_string(json.to_string());
+}
+
+pub fn inputs() -> Inputs {
+    let address1 = String::from("pay:sov:iTQzpdRdugkJ2gLD5vW5c159dncSL9jbAtu3WfPcb8qWD9bUd");
+    let address2 = String::from("pay:sov:BUoojqSQTLuvjkun4y2YoseVF76UZ3uYfHF1dbQyZVbCuTwQo");
+
+    return vec![
+        Input::new(address1, 1),
+        Input::new(address2, 1),
+    ];
+}
+
+pub fn outputs() -> Outputs {
+    let address1 = String::from("pay:sov:2naF9c9ZJnSRtpaptpyZxi18tfozozqFRTmxHk9M6wbBc68T9A");
+    let address2 = String::from("pay:sov:YissN67riFhQ8W6viqtJoCRHFkXtqxaxeL9UyvCoz8sXq5B5A");
+
+    return vec![
+        Output::new(address1, 10, None),
+        Output::new(address2, 22, None),
+    ];
 }
 
 pub fn outputs_json_pointer() -> *const c_char {
@@ -52,4 +75,41 @@ pub fn set_fees_json() -> *const c_char {
     });
 
     return c_pointer_from_string(json.to_string());
+}
+
+pub fn xfer_payload_unsigned() -> XferPayload {
+    let inputs = inputs();
+    let outputs = outputs();
+    return XferPayload::new(inputs, outputs);
+}
+
+pub fn xfer_payload_signed() -> XferPayload {
+    let ver1 = "iTQzpdRdugkJ2gLD5vW5c159dncSL9jbAtu3WfPcb8qWD9bUd";
+    let ver2 = "BUoojqSQTLuvjkun4y2YoseVF76UZ3uYfHF1dbQyZVbCuTwQo";
+    let ver3 = "2naF9c9ZJnSRtpaptpyZxi18tfozozqFRTmxHk9M6wbBc68T9A";
+    let ver4 = "YissN67riFhQ8W6viqtJoCRHFkXtqxaxeL9UyvCoz8sXq5B5A";
+
+    let sig1 = "2T9TfJvLg2EkfJRFvN8D9maUEwEBhvg6eCiFL6PUobgzhTXE1m6y1w7KKEw8MQaUPBkgM2APMdwmMM26UYUatmjd";
+    let sig2 = "2rUrhusR7TmkFs9cyNeHoq2EZ6LQH2RvKSZnJMPJHRSEDAb3aj4GxkvX79JASiHLxMmtz1stu4ysjXpUYZGVCSvr";
+
+    let inputs = vec![
+        Input::new(String::from(ver1), 1),
+        Input::new(String::from(ver2), 2)
+    ];
+
+    let outputs = vec![
+        Output::new(String::from(ver3), 10, None),
+        Output::new(String::from(ver4), 22, None)
+    ];
+
+    let signatures = Some(vec![
+        String::from(sig1),
+        String::from(sig2),
+    ]);
+
+    return XferPayload {
+        inputs,
+        outputs,
+        signatures
+    };
 }
