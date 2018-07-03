aaa3b3d0ea0fa374ce216b3ecfb86788e63ecbd4
dastardlychimp <darien.hess@evernym.com>
2018-07-03 13:36:51 -0600

    Use json_c_pointer! in default.rs

--- libsovtoken/src/utils/mod.rs
+++ libsovtoken/src/utils/mod.rs
@@ -6,7 +6,7 @@ pub mod base58;
 pub mod constants;
 #[macro_use] pub mod ffi_support;
 pub mod general;
-pub mod json_conversion;
+#[macro_use] pub mod json_conversion;
 #[macro_use] pub mod logger;
 pub mod random;
 
