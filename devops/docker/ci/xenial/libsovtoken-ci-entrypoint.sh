ec5cb4442792abef94deedcb7570223e9f214363
Andrey Kononykhin <andkononykhin@gmail.com>
2018-07-12 19:37:12 +0300

    fixes pool init and run for different docker users
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- devops/docker/ci/xenial/libsovtoken-ci-entrypoint.sh
+++ devops/docker/ci/xenial/libsovtoken-ci-entrypoint.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-/usr/bin/supervisord &
+GEN_ARGS="--nodes 4 --clients 5 --nodeNum 1 2 3 4 --ips=$POOL_IP,$POOL_IP,$POOL_IP,$POOL_IP"
+
+if [[ "$(id -u)" = '0' ]]; then
+    gosu indy generate_indy_pool_transactions $GEN_ARGS
+    gosu indy /usr/bin/supervisord &
+else
+    generate_indy_pool_transactions $GEN_ARGS
+    /usr/bin/supervisord &
+fi
 
 exec "$@"
