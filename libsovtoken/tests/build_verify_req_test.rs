72034f8a149128f9ac9f512cc51df3c68a3c8914
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2018-07-24 21:28:37 +0300

    TOK-277 Positive system tests for verify
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- libsovtoken/tests/build_verify_req_test.rs
+++ libsovtoken/tests/build_verify_req_test.rs
@@ -20,15 +20,101 @@ pub fn build_and_submit_verify_on_mint() {
     let payment_addresses = &setup.addresses;
     let pool_handle = setup.pool_handle;
     let dids = setup.trustees.dids();
-    let txo
+    let txo = utils::payment::get_utxo::get_first_utxo_txo_for_payment_address(&wallet, pool_handle, dids[0], &payment_addresses[0]);
 
-    let (get_utxo_req, payment_method) = indy::payments::Payment::build_verify_req(wallet.handle, dids[0], &payment_addresses[0]).unwrap();
+    let (get_utxo_req, payment_method) = indy::payments::Payment::build_verify_req(wallet.handle, dids[0], &txo).unwrap();
     let res = indy::ledger::Ledger::sign_and_submit_request(pool_handle, wallet.handle, dids[0], &get_utxo_req).unwrap();
-    let res = indy::payments::Payment::parse_get_payment_sources_response(&payment_method, &res).unwrap();
+    let res = indy::payments::Payment::parse_verify_response(&payment_method, &res).unwrap();
 
-    let res_parsed: Vec<serde_json::Value> = serde_json::from_str(&res).unwrap();
-    assert_eq!(res_parsed.len(), 1);
-    let utxo = res_parsed.get(0).unwrap().as_object().unwrap();
-    assert_eq!(utxo.get("recipient").unwrap().as_str().unwrap(), payment_addresses[0]);
-    assert_eq!(utxo.get("amount").unwrap().as_u64().unwrap(), 10);
+    println!("{:?}", res);
+//    let res_parsed: serde_json::Value = serde_json::from_str(&res).unwrap();
+//    assert!(res_parsed.as_object().unwrap().get("sources").is_none());
+//    assert!(res_parsed.as_object().unwrap().get("recipients").unwrap().as_object().get("receipt").unwrap().)
+}
+
+#[test]
+pub fn build_and_submit_verify_on_xfer() {
+    let wallet = Wallet::new();
+    let setup = Setup::new(&wallet, SetupConfig {
+        num_addresses: 2,
+        num_trustees: 4,
+        num_users: 0,
+        mint_tokens: Some(vec![10]),
+        fees: None
+    });
+    let payment_addresses = &setup.addresses;
+    let pool_handle = setup.pool_handle;
+    let dids = setup.trustees.dids();
+    let txo = utils::payment::get_utxo::get_first_utxo_txo_for_payment_address(&wallet, pool_handle, dids[0], &payment_addresses[0]);
+
+    let inputs = json!([txo]).to_string();
+    let outputs = json!([
+        {
+            "recipient": payment_addresses[1],
+            "amount": 10
+        }
+    ]).to_string();
+    let (req, method) = indy::payments::Payment::build_payment_req(wallet.handle, dids[0], &inputs, &outputs, None).unwrap();
+    let res = indy::ledger::Ledger::submit_request(pool_handle, &req).unwrap();
+    let res = indy::payments::Payment::parse_payment_response(&method, &res).unwrap();
+
+    let res_parsed: serde_json::Value = serde_json::from_str(&res).unwrap();
+    let utxos = res_parsed.as_array().unwrap();
+    let value = utxos.get(0).unwrap().as_object().unwrap();
+    let new_utxo = value.get("receipt").unwrap().as_str().unwrap();
+
+    let (get_utxo_req, payment_method) = indy::payments::Payment::build_verify_req(wallet.handle, dids[0], &new_utxo).unwrap();
+    let res = indy::ledger::Ledger::sign_and_submit_request(pool_handle, wallet.handle, dids[0], &get_utxo_req).unwrap();
+    let res = indy::payments::Payment::parse_verify_response(&payment_method, &res).unwrap();
+
+    println!("{:?}", res);
+//    let res_parsed: serde_json::Value = serde_json::from_str(&res).unwrap();
+//    assert!(res_parsed.as_object().unwrap().get("sources").is_none());
+//    assert!(res_parsed.as_object().unwrap().get("recipients").unwrap().as_object().get("receipt").unwrap().)
+}
+
+#[test]
+pub fn build_and_submit_verify_on_fees() {
+    let wallet = Wallet::new();
+    let setup = Setup::new(&wallet, SetupConfig {
+        num_addresses: 1,
+        num_trustees: 4,
+        num_users: 0,
+        mint_tokens: Some(vec![10]),
+        fees: Some(json!({
+            "1": 1
+        })),
+    });
+    let payment_addresses = &setup.addresses;
+    let pool_handle = setup.pool_handle;
+    let dids = setup.trustees.dids();
+    let txo = utils::payment::get_utxo::get_first_utxo_txo_for_payment_address(&wallet, pool_handle, dids[0], &payment_addresses[0]);
+
+    let inputs = json!([txo]).to_string();
+    let outputs = json!([{
+        "recipient": payment_addresses[0],
+        "amount": 9
+    }]).to_string();
+
+    let (did_new, verkey_new) = indy::did::Did::new(wallet.handle, "{}").unwrap();
+
+    let nym_req = indy::ledger::Ledger::build_nym_request(dids[0], &did_new,  Some(&verkey_new), None, None).unwrap();
+    let nym_req_signed = indy::ledger::Ledger::sign_request(wallet.handle, dids[0], &nym_req).unwrap();
+    let (nym_req_with_fees, pm) = indy::payments::Payment::add_request_fees(wallet.handle, dids[0], &nym_req_signed, &inputs, &outputs, None).unwrap();
+    let nym_resp = indy::ledger::Ledger::submit_request(pool_handle, &nym_req_with_fees).unwrap();
+    let res = indy::payments::Payment::parse_response_with_fees(&pm, &nym_resp).unwrap();
+
+    let res_parsed: serde_json::Value = serde_json::from_str(&res).unwrap();
+    let utxos = res_parsed.as_array().unwrap();
+    let value = utxos.get(0).unwrap().as_object().unwrap();
+    let new_utxo = value.get("receipt").unwrap().as_str().unwrap();
+
+    let (get_utxo_req, payment_method) = indy::payments::Payment::build_verify_req(wallet.handle, dids[0], &txo).unwrap();
+    let res = indy::ledger::Ledger::sign_and_submit_request(pool_handle, wallet.handle, dids[0], &get_utxo_req).unwrap();
+    let res = indy::payments::Payment::parse_verify_response(&payment_method, &res).unwrap();
+
+    println!("{:?}", res);
+//    let res_parsed: serde_json::Value = serde_json::from_str(&res).unwrap();
+//    assert!(res_parsed.as_object().unwrap().get("sources").is_none());
+//    assert!(res_parsed.as_object().unwrap().get("recipients").unwrap().as_object().get("receipt").unwrap().)
 }
