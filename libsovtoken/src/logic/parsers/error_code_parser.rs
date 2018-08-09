a3fe5563dd764a2baec78e2c5cfaf356a22c5df9
lovesh harchandani <lovesh.bond@gmail.com>
2018-08-09 19:00:34 +0530

    add new error code
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- libsovtoken/src/logic/parsers/error_code_parser.rs
+++ libsovtoken/src/logic/parsers/error_code_parser.rs
@@ -1,12 +1,15 @@
 use indy::ErrorCode;
 
 const INSUFFICIENT_FUNDS_ERROR: &str = "InsufficientFundsError";
+const EXTRA_FUNDS_ERROR: &str = "ExtraFundsError";
 const UTXO_ALREADY_SPENT: &str = "UTXOAlreadySpentError";
 
 pub fn parse_error_code_from_string(reason: &str) -> ErrorCode {
     error!("{}", reason);
     if reason.contains(INSUFFICIENT_FUNDS_ERROR) {
         ErrorCode::PaymentInsufficientFundsError
+    } else if reason.contains(EXTRA_FUNDS_ERROR) {
+        ErrorCode::PaymentExtraFundsError
     } else if reason.contains(UTXO_ALREADY_SPENT) {
         ErrorCode::PaymentSourceDoesNotExistError
     } else {
