6430312f9bcb57bc9608cda6c13d10b7b2bc50ff
dastardlychimp <darien.hess@evernym.com>
2018-08-13 15:31:30 -0600

    Change `UTXOAlreadySpentError` to `InvalidFundsError`.

--- libsovtoken/src/logic/parsers/error_code_parser.rs
+++ libsovtoken/src/logic/parsers/error_code_parser.rs
@@ -1,13 +1,13 @@
 use indy::ErrorCode;
 
 const INSUFFICIENT_FUNDS_ERROR: &str = "InsufficientFundsError";
-const UTXO_ALREADY_SPENT: &str = "UTXOAlreadySpentError";
+const INVALID_FUNDS: &str = "InvalidFundsError";
 
 pub fn parse_error_code_from_string(reason: &str) -> ErrorCode {
     error!("{}", reason);
     if reason.contains(INSUFFICIENT_FUNDS_ERROR) {
         ErrorCode::PaymentInsufficientFundsError
-    } else if reason.contains(UTXO_ALREADY_SPENT) {
+    } else if reason.contains(INVALID_FUNDS) {
         ErrorCode::PaymentSourceDoesNotExistError
     } else {
         ErrorCode::CommonInvalidStructure
