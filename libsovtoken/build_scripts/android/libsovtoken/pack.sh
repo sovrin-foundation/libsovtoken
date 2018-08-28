7e54be0b83c4e8e1efb36107ecc4422ef05d99f7
Andrey Kononykhin <andkononykhin@gmail.com>
2018-08-28 11:29:32 +0300

    adds env to configure package name
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- libsovtoken/build_scripts/android/libsovtoken/pack.sh
+++ libsovtoken/build_scripts/android/libsovtoken/pack.sh
@@ -5,10 +5,11 @@ set -ex
 _PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 TARGET_DIR=$1
+PACKAGE_NAME=${PACKAGE_NAME:-libsovtoken}
 BUILD_TIME=${BUILD_TIME:-$(date -u "+%Y%m%d%H%M")}
 GIT_REV=${GIT_REV:-$(git rev-parse --short HEAD)}
 LIBSOVTOKEN_VER=${LIBSOVTOKEN_VER:-$(grep ^version "${_PWD}/../../../Cargo.toml" | head -n 1 | cut -d '"' -f 2)}
 
 command pushd $(dirname ${TARGET_DIR})
-zip -r "libsovtoken_${LIBSOVTOKEN_VER}-${BUILD_TIME}-${GIT_REV}_all.zip" $(basename ${TARGET_DIR})
+zip -r "${PACKAGE_NAME}_${LIBSOVTOKEN_VER}-${BUILD_TIME}-${GIT_REV}_all.zip" $(basename ${TARGET_DIR})
 command popd
