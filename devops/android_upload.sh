10aa7a3d64051df6bd0ac7e0c1254e7277e333c8
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2019-03-21 15:46:19 +0300

    Modify android upload script
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- devops/android_upload.sh
+++ devops/android_upload.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 if [ "$1" = "--help" ] ; then
-  echo "Usage: <version> <key> <branchName> <number> <artifact_name>"
+  echo "Usage: <version> <key> <branchName> <number> <artifact> <artifact_name>"
   return
 fi
 
@@ -13,17 +13,19 @@ key="$2"
 branchName="$3"
 buildNumber="$4"
 artifact="$5"
+file="$6"
 
 [ -z $version ] && exit 1
 [ -z $key ] && exit 2
 [ -z $branchName ] && exit 3
 [ -z $buildNumber ] && exit 4
 [ -z $artifact ] && exit 5
+[ -z $file ] && exit 6
 
 ssh -v -oStrictHostKeyChecking=no -i $key repo@$SOVRIN_REPO_HOST mkdir -p /var/repository/repos/android/${artifact}/${branchName}/${version}-${buildNumber}
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@$SOVRIN_REPO_HOST
 cd /var/repository/repos/android/${artifact}/${branchName}/$version-$buildNumber
-put -r ${artifact}
+put -r ${file}
 ls -l /var/repository/repos/android/${artifact}/${branchName}/$version-$buildNumber
 EOF
