e55b8886f7f60be49f695c7c5801f5e309e5f40c
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2019-03-21 13:45:19 +0300

    Add android upload script
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

---
+++ devops/android_upload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ "$1" = "--help" ] ; then
+  echo "Usage: <version> <key> <branchName> <number> <artifact_name>"
+  return
+fi
+
+version="$1"
+key="$2"
+branchName="$3"
+buildNumber="$4"
+artifact="$5"
+
+[ -z $version ] && exit 1
+[ -z $key ] && exit 2
+[ -z $branchName ] && exit 3
+[ -z $buildNumber ] && exit 4
+[ -z $artifact ] && exit 5
+
+ssh -v -oStrictHostKeyChecking=no -i $key repo@$SOVRIN_REPO_HOST mkdir -p /var/repository/repos/android/${artifact}/${branchName}/${version}-${buildNumber}
+
+cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@$SOVRIN_REPO_HOST
+cd /var/repository/repos/android/${artifact}/${branchName}/$version-$buildNumber
+put -r ${artifact}
+ls -l /var/repository/repos/android/${artifact}/${branchName}/$version-$buildNumber
+EOF
