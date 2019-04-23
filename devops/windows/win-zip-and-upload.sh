1a384a5b41473b74e4b5ed385033107a7aac231a
artem.ivanov <artem.ivanov@dsr-company.com>
2019-04-23 08:24:46 +0300

    ST-316: Sovtoken CD pipeline
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- devops/windows/win-zip-and-upload.sh
+++ devops/windows/win-zip-and-upload.sh
@@ -24,6 +24,7 @@ TEMP_ARCH_DIR=./${PACKAGE_NAME}-zip
 mkdir ${TEMP_ARCH_DIR}
 
 cp ./target/release/*.dll ${TEMP_ARCH_DIR}/
+cp ./target/release/*.dll.lib ${TEMP_ARCH_DIR}/
 
 pushd ${TEMP_ARCH_DIR}
     zip -r ${PACKAGE_NAME}_${version}.zip ./*
@@ -33,10 +34,6 @@ popd
 rm -rf ${TEMP_ARCH_DIR}
 
 cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@$SOVRIN_REPO_HOST
-mkdir /var/repository/repos/windows/$PACKAGE_NAME
-mkdir /var/repository/repos/windows/$PACKAGE_NAME/master
-mkdir /var/repository/repos/windows/$PACKAGE_NAME/stable
-mkdir /var/repository/repos/windows/$PACKAGE_NAME/test
 mkdir /var/repository/repos/windows/$PACKAGE_NAME/$type/$version-$number
 cd /var/repository/repos/windows/$PACKAGE_NAME/$type/$version-$number
 put -r ${PACKAGE_NAME}_"${version}".zip
