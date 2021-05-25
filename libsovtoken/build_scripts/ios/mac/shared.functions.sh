db9db93e9cb40ccb2f4c54a8cb4ac6387b637ba9
Ian Costanzo <ian@anon-solutions.ca>
2021-05-25 11:06:49 -0700

    Update ci/cd to use indy sdk 1.16.0
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>
    Signed-off-by: Devin Smith <devinleighsmith@gmail.com>

--- libsovtoken/build_scripts/ios/mac/shared.functions.sh
+++ libsovtoken/build_scripts/ios/mac/shared.functions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/stable/libindy-core/1.14.0/libindy.tar.gz"
+export LIBINDY_IOS_BUILD_URL="https://repo.sovrin.org/ios/libindy/stable/libindy-core/1.16.0/libindy.tar.gz"
 export LIBINDY_FILE=$(basename ${LIBINDY_IOS_BUILD_URL})
 export LIBINDY_VERSION=$(basename $(dirname ${LIBINDY_IOS_BUILD_URL}))
 export BUILD_CACHE=~/.build_libvxc/ioscache
